a66d9b0e8dcbe5ea36ddfa21570b6d00c50d4616
Nathat <nathat890@outlook.com>
2015-11-17 21:03:34 +0000

    Fixed NullPointerException when the stream is offline

--- src/main/java/com/outlook/nathat890/twitchhelper/TwitchHelper.java
+++ src/main/java/com/outlook/nathat890/twitchhelper/TwitchHelper.java
@@ -3,17 +3,20 @@
  */
 
 package com.outlook.nathat890.twitchhelper;
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.InputStream;
 
 public class TwitchHelper{
 
-    public static String version = "v0.0.1";
+    public static String version = "v0.0.2-DEV";
     public GetData data = new GetData();
     public Thread myThread = new Thread(data);
-    JLabel frame = new JLabel("Details: ");
     public static JLabel game = new JLabel("Game: ");
     public static JLabel mature = new JLabel("Mature: ");
     public static JLabel creation = new JLabel("Account Created: ");
@@ -30,27 +33,40 @@ public class TwitchHelper{
         return version;
     }
 
+
     public static void main(String[] args){
         try {
             TwitchHelper t = new TwitchHelper();
             System.out.println("TwitchHelper " + getVersion() + " by @nathat890");
             JFrame main = new JFrame("TwitchHelper " + getVersion());
             main.setResizable(false);
-            main.setPreferredSize(new Dimension(550, 225));
+            main.setPreferredSize(new Dimension(700, 250));
+            ImageIcon img = new ImageIcon("icon.jpg");
+            main.setIconImage(img.getImage());
             text.setMaximumSize(new Dimension(300, 27));
             main.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
             JPanel panelt = new JPanel();
             JPanel panela = new JPanel();
             JPanel mainp = new JPanel(new BorderLayout());
             panelt.setLayout(new BoxLayout(panelt, BoxLayout.X_AXIS));
+            JLabel title = new JLabel("TwitchHelper");
+            Font bold = new Font("Verdana", Font.BOLD, 30);
+            panelt.setSize(new Dimension(350, 100));
+            title.setFont(bold);
+            title.setForeground(Color.WHITE);
+            panelt.add(title);
+            title.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
             panelt.add(text);
+            text.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
             panelt.add(check);
+            panelt.setBackground(Color.DARK_GRAY);
+            panela.setBackground(Color.GRAY);
             main.getContentPane().setLayout(new BoxLayout(main.getContentPane(), BoxLayout.Y_AXIS));
             mainp.add(panelt,BorderLayout.PAGE_START);
             mainp.add(panela,BorderLayout.CENTER);
-            panelt.setSize(new Dimension(350, 25));
             panela.setLayout(new BoxLayout(panela, BoxLayout.Y_AXIS));
             main.getContentPane().add(mainp);
+            timereset.setForeground(Color.WHITE);
             panela.add(timereset);
             check.addActionListener(new ActionListener() {
                 public void actionPerformed(ActionEvent event) {
@@ -60,7 +76,14 @@ public class TwitchHelper{
                 }
 
             });
-            panela.add(t.frame);
+            status.setForeground(Color.WHITE);
+            currentviews.setForeground(Color.WHITE);
+            fps.setForeground(Color.WHITE);
+            views.setForeground(Color.WHITE);
+            followers.setForeground(Color.WHITE);
+            game.setForeground(Color.WHITE);
+            mature.setForeground(Color.WHITE);
+            creation.setForeground(Color.WHITE);
             panela.add(status);
             panela.add(currentviews);
             panela.add(fps);
